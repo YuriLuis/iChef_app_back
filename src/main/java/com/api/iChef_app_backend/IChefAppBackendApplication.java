88950c306c68e551951d6b8fe3379031d9d84e00
Milton <milton.dev.sis@gmail.com>
2023-01-20 16:27:21 -0300

    first commit

---
+++ src/main/java/com/api/iChef_app_backend/IChefAppBackendApplication.java
@@ -0,0 +1,12 @@
+package com.api.iChef_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IChefAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IChefAppBackendApplication.class, args);
+	}
+}
